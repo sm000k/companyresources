63d7cea40273dbce00952630d254bc4a9f9e5bc9
pwlac <smok.lacki@gmail.com>
2025-05-10 15:15:45 +0200

    initial commit

---
+++ src/main/java/com/placki/companyresources/CompanyresourcesApplication.java
@@ -0,0 +1,13 @@
+package com.placki.companyresources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyresourcesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyresourcesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/placki/companyresources/CompanyresourcesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.placki.companyresources;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyresourcesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
